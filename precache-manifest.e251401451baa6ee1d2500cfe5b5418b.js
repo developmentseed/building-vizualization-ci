1a1e265fe630c1dc18fd5eb0b2127c91cd56938d
Rub21 <rub2106@gmail.com>
2018-10-18 15:50:26 -0500

    Updates

---
+++ precache-manifest.e251401451baa6ee1d2500cfe5b5418b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3d62f8a78f0422981526",
+    "url": "/static/js/main.3d62f8a7.chunk.js"
+  },
+  {
+    "revision": "13f66728caea7fb6e6f0",
+    "url": "/static/js/1.13f66728.chunk.js"
+  },
+  {
+    "revision": "3d62f8a78f0422981526",
+    "url": "/static/css/main.5bc69156.chunk.css"
+  },
+  {
+    "revision": "13f66728caea7fb6e6f0",
+    "url": "/static/css/1.adab6513.chunk.css"
+  },
+  {
+    "revision": "3455c0744555ed7ff723b605a73fcd93",
+    "url": "/index.html"
+  }
+];
