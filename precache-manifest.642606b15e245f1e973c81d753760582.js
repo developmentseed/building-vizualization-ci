8f3b904a5021446b516514b047b7142852d38507
piligab <pilar@developmentseed.org>
2018-10-31 14:58:12 -0500

    Updates

---
+++ precache-manifest.642606b15e245f1e973c81d753760582.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c12a249e2c29bbadc027",
+    "url": "/static/js/main.c12a249e.chunk.js"
+  },
+  {
+    "revision": "7b8f893580518776895b",
+    "url": "/static/js/1.7b8f8935.chunk.js"
+  },
+  {
+    "revision": "c12a249e2c29bbadc027",
+    "url": "/static/css/main.5d5b3b0f.chunk.css"
+  },
+  {
+    "revision": "7b8f893580518776895b",
+    "url": "/static/css/1.93040696.chunk.css"
+  },
+  {
+    "revision": "6324f30dbb48cacc10dd844dbd371ab1",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7f240a4a04bbe09a61259a6701e94ef1.js"
+  "/precache-manifest.642606b15e245f1e973c81d753760582.js"
 );
 
 workbox.clientsClaim();
